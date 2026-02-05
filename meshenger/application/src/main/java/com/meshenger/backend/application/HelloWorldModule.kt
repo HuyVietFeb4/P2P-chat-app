4a08b36161ee9ee5d67a1901d0ae8c67bcdad9c5
Huy Viet <nguyentranhuyviet04022004@gmail.com>
2026-02-05 22:15:48 +0700

    Done project structure

--- meshenger/application/src/main/java/com/meshenger/backend/application/HelloWorldModule.kt
+++ meshenger/application/src/main/java/com/meshenger/backend/application/HelloWorldModule.kt
@@ -14,13 +14,11 @@ class HelloWorldModuleApplication(reactContext: ReactApplicationContext) :
 
     override fun getName(): String = "HelloWorldModuleApplication"
 
-    // Exposed to JS
     @ReactMethod
     fun getMessage(promise: Promise) {
         promise.resolve("Application layer says: ${HelloWorld.getMessage()}")
     }
 
-    // Also exposed to JS
     @ReactMethod
     fun getMessageSession(promise: Promise) {
         promise.resolve(bridge.getMessage())
--- meshenger/session/src/main/java/com/meshenger/backend/session/HelloWordBridge.kt
+++ meshenger/session/src/main/java/com/meshenger/backend/session/HelloWordBridge.kt
@@ -3,7 +3,7 @@ package com.meshenger.backend.session
 class HelloWorldBridge {
     companion object {
         init {
-            System.loadLibrary("session") // name of your .so library
+            System.loadLibrary("session")
         }
     }
 
