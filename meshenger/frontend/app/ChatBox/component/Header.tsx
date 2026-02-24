0d8a62e657731478ee3805d3e35b5c21875b2311
Lmao1232j <vinhtran23042004@gmail.com>
2026-02-24 15:04:58 +0700

    Finish header

---
+++ meshenger/frontend/app/ChatBox/component/Header.tsx
@@ -0,0 +1,131 @@
+import { Image } from "expo-image";
+import { LinearGradient } from "expo-linear-gradient";
+import { LayoutGrid, Search, ShieldAlert, UserPlus, UserRound, Users } from "lucide-react-native";
+import { StyleSheet, Text, View, useWindowDimensions } from "react-native";
+
+export default function Header() {
+    const {width, height} = useWindowDimensions();
+    return (
+        <LinearGradient
+            colors={['#0F4C81', '#5F2EEA']}
+            start={{x: 0, y: 0}}
+            end={{x: 1, y: 0}}
+            style={{width: width, height: height * 0.2}}
+        >
+            <View style={[styles.headerContainer, {width: width * 0.9}]}>
+                <View style={styles.actionContainer}> 
+                    <View style={styles.appTitleContainer}>
+                        <Image
+                            source={require('@/assets/images/appIcon.png')}
+                            style={styles.imageStyle}
+                            contentFit="cover"
+                        />
+                        <Text style={styles.headerText}>Meshenger</Text>
+                    </View>
+
+                    <View style={styles.actionIconsContainer}>
+                        <Search
+                            size={22}
+                            color="#fff"
+                        />
+
+                        <UserPlus
+                            size={22}
+                            color='#fff'
+                        />
+                    </View>
+                </View>
+
+                <View style={styles.filterContainer}>
+                    <View style={styles.filterFieldContainer}>
+                        <LayoutGrid
+                            size={12}
+                            color='#5F2EEA'
+                        />
+                        <Text style={styles.filterText}>All</Text>
+                    </View>
+
+                    <View style={styles.filterFieldContainer}>
+                        <UserRound
+                            size={12}
+                            color='#5F2EEA'
+                        />
+                        <Text style={styles.filterText}>Individual</Text>
+                    </View>
+
+                    <View style={styles.filterFieldContainer}>
+                        <Users
+                            size={12}
+                            color='#5F2EEA'
+                        />
+                        <Text style={styles.filterText}>Group</Text>
+                    </View>
+
+                    <View style={styles.filterFieldContainer}>
+                        <ShieldAlert
+                            size={12}
+                            color='#5F2EEA'
+                        />
+                        <Text style={styles.filterText}>Emergency</Text>
+                    </View>
+                </View>
+            </View>
+        </LinearGradient>
+    );
+}
+
+const styles = StyleSheet.create({
+    imageStyle: {
+        width: 30,
+        height: 32
+    },
+
+    headerText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#fff'
+    },
+
+    appTitleContainer: {
+        flexDirection: 'row',
+        gap: 10
+    },
+
+    actionIconsContainer: {
+        flexDirection: 'row',
+        gap: 20,
+    },
+
+    actionContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 25
+    },
+
+    headerContainer: {
+        alignSelf: 'center',
+        marginTop: 'auto',
+        paddingBottom: 5
+    },
+
+    filterText: {
+        fontSize: 12,
+        color: '#5F2EEA'
+    },
+
+    filterFieldContainer: {
+        flexDirection: 'row',
+        gap: 5,
+        alignItems: 'center'
+    },
+
+    filterContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: 'rgba(233, 230, 255, 0.8)',
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        borderRadius: 20,
+    }
+});
---
+++ meshenger/session/build/intermediates/cxx/Debug/6960y5g3/logs/arm64-v8a/build_command_session.bat
@@ -0,0 +1,5 @@
+@echo off
+"D:\\AndroidSDK\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "D:\\P2P-chat-app\\meshenger\\session\\.cxx\\Debug\\6960y5g3\\arm64-v8a" ^
+  session
