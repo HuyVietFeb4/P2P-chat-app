8cc3d9c61cb09df1c16a4eafae4def2d8057602c
Lmao1232j <vinhtran23042004@gmail.com>
2026-02-25 17:03:12 +0700

    Finish footer layout

--- meshenger/frontend/app/ChatBox/index.tsx
+++ meshenger/frontend/app/ChatBox/index.tsx
@@ -1,10 +1,13 @@
 import { View } from "react-native";
 import Header from "./component/Header";
+import Footer from "./component/Footer";
 
 export default function ChatBox() {
     return (
         <View style={{flex: 1}}>
             <Header />
+            <View style={{flex: 1}} />
+            <Footer />
         </View>
     );
 }
